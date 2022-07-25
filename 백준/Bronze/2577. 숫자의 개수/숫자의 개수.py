a70f524c11477561134655720a05cfa64d63cf4d
최보영 <62585191+jupiter6676@users.noreply.github.com>
2022-07-25 13:41:43 +0900

    [Bronze II] Title: 숫자의 개수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+num_cnt = [0] * 10  # 0 ~ 9
+abc = str(a * b * c)
+
+for num in abc:
+    num_cnt[int(num)] += 1
+
+for cnt in num_cnt:
+    print(cnt)
