62bba3569f33ab90f5cffb7ab92e49d73b69e501
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2022-07-28 15:08:22 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.py
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.py
@@ -1,17 +1,6 @@
-def rev(x):
-    rev_arr = list()
+x, y = input().split()
 
-    while x > 0:
-        rev_arr.append(x % 10)
-        x //= 10
+sum_ = str(int(x[::-1]) + int(y[::-1]))
+rev_sum = int(sum_[::-1])
 
-    rev_num = 0
-    for i in range(len(rev_arr)):
-        digit_for_i = len(rev_arr) - (i + 1)
-        rev_num += rev_arr[i] * (10 ** digit_for_i)
-
-    return rev_num
-
-x, y = map(int, input().split())
-res = rev(rev(x) + rev(y))
-print(res)
+print(rev_sum)
