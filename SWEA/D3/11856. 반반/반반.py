26c70688f7d61f466e4d22fd7ec6697674b6d96f
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2022-08-13 16:58:43 +0900

    [D3] Title: 반반, Time: 157 ms, Memory: 58,508 KB -BaekjoonHub

---
+++ SWEA/D3/11856. 반반/반반.py
@@ -0,0 +1,20 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    S = input()
+    dic = dict()
+
+    for ch in S:
+        dic[ch] = dic.get(ch, 0) + 1
+
+    print(f'#{tc}', end=' ')
+    
+    if len(dic) == 2:
+        for k, v in dic.items():
+            if v != 2:
+                print('No')
+                break
+        else:
+            print('Yes')
+    else:
+        print('No')
