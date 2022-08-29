407021bc2e8ec018a4c8cd499a2129d754f7a205
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2022-08-29 23:19:26 +0900

    [Silver IV] Title: 개미, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.py
+++ 백준/Silver/10158. 개미/개미.py
@@ -2,9 +2,6 @@ w, h = map(int, input().split())
 p, q = map(int, input().split())
 t = int(input())
 
-NE = 1  # 오른쪽 위 45도 방향
-SW = 0  # 왼쪽 아래 45도 방향
-
 x_cnt = (p + t) // w    # x좌표 왕복 횟수
 y_cnt = (q + t) // h    # y좌표 왕복 횟수
 
