cedaac55d4b88127fdc5643ffe7904c4a765b263
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2022-09-09 16:12:03 +0900

    [D2] Title: 파리 퇴치, Time: 162 ms, Memory: 61,732 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,25 +1,24 @@
+# import sys
+
+# sys.stdin = open("input.txt", "r")
+
 T = int(input())
 
-for test_case in range(1, T + 1):
+for t in range(1, T + 1):
     N, M = map(int, input().split())
-    lst = [list(map(int, input().split())) for _ in range(N)]
+    graph = [list(map(int, input().split())) for _ in range(N)]
 
-    max_ = 0
+    max_sum = 0
 
     for i in range(N):
         for j in range(N):
-            tmp = 0
+            tmp_sum = 0
 
-            # 현재 위치에서, 가로 세로 + (m - 1)만큼
-            # 즉, 가로 m, 세로 m의 직사각형 내의 합 구하기
-            for dy in range(M):
-                for dx in range(M):
-                    # 배열의 인덱스 범위 내에서 합 구하기
-                    if i + dy < N and j + dx < N:
-                        tmp += lst[i + dy][j + dx]
+            for y in range(M):
+                for x in range(M):
+                    if not (i + y >= N or j + x >= N):
+                        tmp_sum += graph[i + y][j + x]
 
-                    # 합의 최댓값 갱신
-                    if tmp > max_:
-                        max_ = tmp
+            max_sum = max(max_sum, tmp_sum)
 
-    print(f'#{test_case} {max_}')
+    print(f'#{t} {max_sum}')
