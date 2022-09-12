1d3814d61bb5eed3b967613749885d4577de569d
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2022-09-12 16:33:42 +0900

    [D3] Title: 민석이의 과제 체크하기, Time: 399 ms, Memory: 64,624 KB -BaekjoonHub

--- SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
+++ SWEA/D3/5431. 민석이의 과제 체크하기/민석이의 과제 체크하기.py
@@ -1,21 +1,15 @@
 T = int(input())
 
-for test_case in range(1, T + 1):
-    # N은 총 학생, K는 과제 제출한 학생
+for t in range(1, T + 1):
     N, K = map(int, input().split())
-    # 과제를 제출한 학생 입력 (0 ~ N - 1)
-    lst = list(map(int, input().split()))
+    lst_1 = [False] * (N + 1) # 모든 학생의 과제 제출 여부
+    lst_2 = list(map(int, input().split()))   # 과제를 제출한 사람
 
-    # 과제한 학생 체크 (1 ~ N)
-    check = [0] * (N + 1)
+    for i in lst_2:
+        lst_1[i] = True
 
-    # 과제한 학생 번호 -> check 배열 idx
-    for stu in lst:
-        check[stu] = 1
-
-    # check 출력
-    print(f'#{test_case}', end=' ')
+    print(f'#{t}', end=' ')
     for i in range(1, N + 1):
-        if check[i] == 0:
+        if not lst_1[i]:
             print(i, end=' ')
     print()
