cfb70f4d235c589fe9306d41b72e29a95c812fca
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2022-09-27 17:20:36 +0900

    [D2] Title: 숫자 배열 회전, Time: 129 ms, Memory: 58,316 KB -BaekjoonHub

---
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -0,0 +1,23 @@
+# 시계방향으로 90도 회전
+def rotate(matrix):
+    rotated = [[0] * N for _ in range(N)]
+
+    for i in range(N):
+        for j in range(N):
+            rotated[i][j] = matrix[N - j - 1][i]
+
+    return rotated
+
+
+T = int(input())
+for t in range(1, T + 1):
+    N = int(input())
+    lst = [list(input().split()) for _ in range(N)] # 문자들이 요소
+    
+    rotate_90 = rotate(lst)
+    rotate_180 = rotate(rotate_90)
+    rotate_270 = rotate(rotate_180)
+
+    print(f'#{t}')
+    for i in range(N):
+        print(''.join(rotate_90[i]), ''.join(rotate_180[i]), ''.join(rotate_270[i]))
