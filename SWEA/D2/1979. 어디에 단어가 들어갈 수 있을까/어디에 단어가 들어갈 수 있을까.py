aac84a8853310a01c478bb09302a3655c9424338
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2022-10-01 22:37:59 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 146 ms, Memory: 60,408 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,37 +1,36 @@
 T = int(input())
-
-for test_case in range(1, T + 1):
+for t in range(1, T + 1):
     N, K = map(int, input().split())
-    lst = [list(map(int, input().split())) for _ in range(N)]
+    graph = [list(map(int, input().split())) for _ in range(N)]
+    
+    res = 0
 
-    cnt = 0
     # 가로
     for i in range(N):
         tmp = 0
-
         for j in range(N):
-            # 가로로 이동하면서, 
-            # 흰 칸을 연속적으로 발견할 때마다 tmp +1
-            if lst[i][j] == 1:
+            if graph[i][j] == 1:
                 tmp += 1
-
-            # 검은 칸, 혹은 행의 끝을 만나면
-            if lst[i][j] == 0 or j == N - 1:
-                # 흰 칸의 수와 글자 수 K가 같은지 검사
+            else:
                 if tmp == K:
-                    cnt += 1
-                tmp = 0 # tmp 초기화
+                    res += 1
+                tmp = 0
+
+        if tmp == K:
+            res += 1
 
     # 세로
     for j in range(N):
         tmp = 0
         for i in range(N):
-            if lst[i][j] == 1:
+            if graph[i][j] == 1:
                 tmp += 1
-
-            if lst[i][j] == 0 or i == N - 1:
+            else:
                 if tmp == K:
-                    cnt += 1
+                    res += 1
                 tmp = 0
 
-    print(f'#{test_case} {cnt}')
+        if tmp == K:
+            res += 1
+
+    print(f'#{t} {res}')
