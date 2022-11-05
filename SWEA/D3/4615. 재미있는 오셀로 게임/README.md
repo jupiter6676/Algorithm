8586cd180bf9276f5ee67c267c89e21fde4556a1
jupiter6676 <jupiter6676@naver.com>
2022-11-06 00:55:55 +0900

    [D3] Title: 재미있는 오셀로 게임, Time: 205 ms, Memory: 64,988 KB -BaekjoonHub

---
+++ SWEA/D3/4615. 재미있는 오셀로 게임/README.md
@@ -0,0 +1,13 @@
+# [D3] 재미있는 오셀로 게임 - 4615
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWQmA4uK8ygDFAXj) 
+
+### 성능 요약
+
+메모리: 64,988 KB, 시간: 205 ms, 코드길이: 2,030 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
+
+[D3] Title: 재미있는 오셀로 게임, Time: 205 ms, Memory: 64,988 KB -BaekjoonHub
---
+++ SWEA/D3/4615. 재미있는 오셀로 게임/재미있는 오셀로 게임.py
@@ -0,0 +1,66 @@
+T = int(input())
+
+for t in range(1, T + 1):
+    N, M = map(int, input().split())    # 보드 한 변, 돌 놓는 횟수
+    board = [[0] * N for _ in range(N)]
+
+    BLACK = 1
+    WHITE = 2
+
+    board[N//2 - 1][N//2 - 1] = WHITE
+    board[N//2 - 1][N//2] = BLACK
+    board[N//2][N//2 - 1] = BLACK
+    board[N//2][N//2] = WHITE
+
+    for _ in range(M):
+        x, y, stone = map(int, input().split())
+        x -= 1
+        y -= 1
+
+        board[y][x] = stone
+
+        #
+        dy = [-1, -1, -1, 0, 1, 1, 1, 0]
+        dx = [-1, 0, 1, 1, 1, 0, -1, -1]
+
+        for d in range(8):
+            ny = y + dy[d]
+            nx = x + dx[d]
+
+            visited = list()   # 이미 뒤집힌 돌은 뒤집지 않도록
+
+            while True:
+                if not (0 <= ny < N and 0 <= nx < N):
+                    visited = list()
+                    break
+
+                if board[ny][nx] == 0:
+                    visited = list()
+                    break
+
+                if board[ny][nx] == stone:
+                    for elem in visited:
+                        if stone == WHITE:
+                            board[elem[0]][elem[1]] = WHITE
+                        else:
+                            board[elem[0]][elem[1]] = BLACK
+
+                    break
+
+                else:
+                    visited.append([ny, nx])
+
+                ny += dy[d]
+                nx += dx[d]
+
+    black_cnt = 0
+    white_cnt = 0
+
+    for row in board:
+        for elem in row:
+            if elem == BLACK:
+                black_cnt += 1
+            elif elem == WHITE:
+                white_cnt += 1
+
+    print(f'#{t} {black_cnt} {white_cnt}')
