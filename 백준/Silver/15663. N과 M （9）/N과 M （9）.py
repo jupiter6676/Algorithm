21270acbf658e529a39e342315ef41c79e502b44
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2022-12-21 17:09:08 +0900

    [Silver II] Title: N과 M (9), Time: 92 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,17 +1,23 @@
 def solution():
     if len(seq) == M:
-        if res.get(tuple(seq), 0) == 0:
-            res[tuple(seq)] = 1 # 리스트 끝에 iterable의 모든 항목 넣기
+        print(*seq)
         return
 
-    for i in range(len(lst)):
-        if not visited[i]:
-            seq.append(lst[i])
+    # 4 2
+    # 1 7 9 9
+    # 9 → 9로 새로운 solution을 호출하면서, remember는 9 → 0이 되어, 새로운 9가 들어감
+    # [9, 9]가 되어 그 solution 함수가 종료되면, 이전 9가 호출된 함수의 18번째 줄로 돌아옴
+    # 그러면 remember는 9가 된다..
+    remember = 0
+    for i in range(N):
+        if not visited[i] and remember != lst[i]:
             visited[i] = True
+            seq.append(lst[i])
+            remember = lst[i]
             solution()
 
-            seq.pop()
             visited[i] = False
+            seq.pop()
 
 
 '''main'''
@@ -21,10 +27,4 @@ lst.sort()
 visited = [False] * N   # lst[i] ↔ visited[i]
 seq = list()
 
-# 키로 seq의 튜플
-res = dict()
-
 solution()
-
-for seq in res.keys():
-    print(*seq)
