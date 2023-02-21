bf07d5e3e007fc5a331c7e1e9985f427b9acd05f
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2023-02-21 15:31:31 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 664 ms, Memory: 122144 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+dp = [0] * (1000001)
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for t in range(T):
+    N = int(input())
+
+    for i in range(4, N + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009
+
+    print(dp[N])
