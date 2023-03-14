0eb6e1cacd71add918d04638745ba8903e522203
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2023-03-14 20:34:21 +0900

    [Gold V] Title: 동전, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    N = int(input())
+    coins = list(map(int, input().split()))
+    M = int(input())    # 목표 금액 (1 ~ 10,000)
+
+    dp = [0] * (M + 1)
+    dp[0] = 1
+
+    for c in coins:
+        for i in range(c, M + 1):
+            dp[i] += dp[i - c]
+    
+    print(dp[M])
