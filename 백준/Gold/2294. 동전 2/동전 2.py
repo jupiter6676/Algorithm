487b4c5a363c80f1649bfbd45f39d6ff3f0b5f02
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2023-03-16 18:20:16 +0900

    [Gold V] Title: 동전 2, Time: 352 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -3,21 +3,15 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 coins = set()
-dp = [-1] * (K + 1)
 
 for _ in range(N):
     coins.add(int(input()))
 
+dp = [10001] * (K + 1)
+dp[0] = 0   # 0원을 만드는 동전의 수는 0개
+
 for c in coins:
     for i in range(c, K + 1):
-        # if dp[i] == -1:
-        if i % c == 0:
-            dp[i] = i // c
+        dp[i] = min(dp[i], dp[i - c] + 1)
 
-        if dp[i - c] > 0:
-            if dp[i] == -1:
-                dp[i] = dp[i - c] + 1
-            else:
-                dp[i] = min(dp[i], dp[i - c] + 1)
-
-print(dp[K])
+print(dp[K] if dp[K] != 10001 else -1)
