542a0ee0b6e60841fa068b47ddbe3dfe74dc3584
Boyeong <62585191+jupiter6676@users.noreply.github.com>
2023-05-30 20:27:27 +0900

    [Gold III] Title: 택배, Time: 1480 ms, Memory: 35372 KB -BaekjoonHub

---
+++ 백준/Gold/1719. 택배/택배.py
@@ -0,0 +1,43 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+def pprint(mat):
+    for i in range(1, V + 1):
+        for j in range(1, V + 1):
+            print(mat[i][j] if mat[i][j] != 0 else '-', end=' ')
+        print()
+
+def dijkstra(start):
+    dists = [int(1e9)] * (V + 1)
+    dists[start] = 0
+    
+    q = [[0, start]]
+    while q:
+        w1, v1 = heapq.heappop(q)
+
+        for adj, w2 in graph[v1]:
+            if dists[adj] > w1 + w2:
+                dists[adj] = w1 + w2
+
+                if v1 == start:
+                    path[start][adj] = adj
+                else:
+                    path[start][adj] = path[start][v1]
+
+                heapq.heappush(q, [w1 + w2, adj])
+
+
+V, E = map(int, input().split())
+graph = [[] for _ in range(V + 1)]
+path = [[0] * (V + 1) for _ in range(V + 1)]
+
+for _ in range(E):
+    v1, v2, w = map(int, input().split())
+    graph[v1].append([v2, w])
+    graph[v2].append([v1, w])
+
+for i in range(1, V + 1):
+    dijkstra(i)
+
+pprint(path)
